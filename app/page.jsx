01d3989bae81b31fca5a0638aaec63dc931502f9
franciscobluez <152335313+franciscobluez@users.noreply.github.com>
2023-11-29 10:36:20 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ import TicketCard from "./(components)/TicketCard";
 
 const getTickets = async () => {
   try {
-    const res = await fetch("https://ticketbluz.vercel.app/api/Tickets/", {
+    const res = await fetch("http://localhost:3000/api/Tickets/", {
       cache: "no-store",
     });
 
